c21af508a4774729a4321d4f2cb148d0498e3fc0
CHAT <wochen85@gmail.com>
2019-05-15 16:29:53 +0800

    no message

---
+++ Example/Pods/Target Support Files/Pods-FKTableCollectionExtensionBase_Example/Pods-FKTableCollectionExtensionBase_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FKTableCollectionExtensionBase_Example : NSObject
+@end
+@implementation PodsDummy_Pods_FKTableCollectionExtensionBase_Example
+@end
